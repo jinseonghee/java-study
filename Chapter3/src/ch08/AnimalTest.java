bfc9dbdf8ca56bca546c2555987c5a832ff886ef
jinseonghee <wlstjdgml0610@gmail.com>
2021-06-27 21:56:29 +0900

    다운캐스팅 예제

--- Chapter3/src/ch08/AnimalTest.java
+++ Chapter3/src/ch08/AnimalTest.java
@@ -1,12 +1,15 @@
 package ch08;
 
 
+import com.sun.xml.internal.ws.resources.UtilMessages;
+
 import java.util.ArrayList;
 
 class Animal {
     public void move() {
         System.out.println("동물이 움직입니다.");
     }
+
     public void eating() { //공통으로 사용될 메서드는 상위클래스에 넣어두고 하위클래스에서 사용할 수 있다.
 
     }
@@ -18,28 +21,31 @@ class Human extends Animal {
     public void move() {
         System.out.println("사람이 두 발로 걷습니다.");
     }
+
     public void readBook() {
         System.out.println("사람이 책을 읽습니다.");
     }
 }
 
-class Tiger extends  Animal {
+class Tiger extends Animal {
 
     @Override
     public void move() {
         System.out.println("호랑이가 네 발로 뜁니다.");
     }
+
     public void hunting() {
         System.out.println("호랑이가 사냥을 합니다.");
     }
 }
 
-class Eagle extends  Animal {
+class Eagle extends Animal {
 
     @Override
     public void move() {
         System.out.println("독수리가 네 발로 뜁니다.");
     }
+
     public void flying() {
         System.out.println("독수리가 양날개를 쭉 펴고 날아다닙니다.");
     }
@@ -53,7 +59,7 @@ public class AnimalTest {
         Animal tAnimal = new Tiger();
         Animal eAnimal = new Eagle();
 
-//        AnimalTest test = new AnimalTest();
+        AnimalTest test = new AnimalTest();
 //        test.moveAnimal(hAnimal);//moveAnimal이 Animal 타입이기 때문에 animal로 형변환이 되어 다 사용 가능
 //        test.moveAnimal(tAnimal);
 //        test.moveAnimal(eAnimal);
@@ -63,12 +69,33 @@ public class AnimalTest {
         animalList.add(tAnimal);
         animalList.add(eAnimal);
 
-        for(Animal animal : animalList) {
+        for (Animal animal : animalList) { //다형성
             animal.move();
+        }
+        test.testDownCasting(animalList);
+    }
+
+    public void testDownCasting(ArrayList<Animal> list) { //ArrayList를 매개변수로 주면서 다운 캐스팅
+
+        for (int i = 0; i < list.size(); i++) {
+
+            Animal animal = list.get(i);
+
+            if (animal instanceof Human) {
+                Human human = (Human) animal;
+                human.readBook();
+            } else if (animal instanceof Tiger) {
+                Tiger tiger = (Tiger) animal;
+                tiger.hunting();
+            } else if (animal instanceof Eagle) {
+                Eagle eagle = (Eagle) animal;
+                eagle.flying();
+            } else {
+                System.out.println("unsupported type");
+            }
         }
     }
 
-    ㅔ
     public void moveAnimal(Animal animal) { //매개변수를 animal로 선언
         animal.move();
     }
